8f53bd7515c9ea0f10ab90fea13f5210eacf910c
Sam <samara.amanda01@gmail.com>
2021-07-25 17:34:24 -0400

    removed unneccesary association

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,6 +2,7 @@ class CategorySerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
 
-  has_many :listing_categories
-  has_many :listings, through: :listing_categories
+ 
+  has_many :listing_categories, through: :categories
 end
+
