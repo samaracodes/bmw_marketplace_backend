6ffee0e65ef3a631b182b39db0b06317d380bb12
Sam <samara.amanda01@gmail.com>
2021-12-09 17:52:21 -0500

    database seeded data, file deletions

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,13 @@
 
 # Users
 sam = User.create(name: "Samara A", email:"sam@#samara.com", password_digest: "sam", birthday: "June 10", location: "New York")
+------------------------------------------------------------
 
 # Listings
-listing1 = Listing.create(title: "HRE 20inch 305 Wheels", description: "Brand new in box HRE 305's. Fitment: 20x9.5 and 20x11. Can meet up in NYC", price: 5000.0, listing_type: "Buy it now", condition: "New", status: "Active", duration: "3 days", user_id: 1)
+wheel_listing = Listing.create(title: "HRE 20inch 305 Wheels", description: "Brand new in box HRE 305's. Fitment: 20x9.5 and 20x11. Can meet up in NYC", price: 5000.0, listing_type: "Buy it now", condition: "New", status: "Active", duration: "3 days", user_id: 1)
+
+car_listing = Listing.create(title:"2018 BMW F80 M3", description:"Tastefully modded Santorini Blue M3 up for sale. 15k miles, never tracked or abused.", price: 65000.0, listing_type: "Giveaway", condition:"New", status:"Active", duration: "30 days", user_id: 1)
+------------------------------------------------------------
 
 # Category
 car = Category.create(name: "Car")
@@ -19,10 +23,15 @@ parts = Category.create(name: "Parts")
 wheels = Category.create(name: "Wheels/Tires")
 accessories = Category.create(name: "Accessories")
 tools = Category.create(name: "Tools")
-
+------------------------------------------------------------
 
 # Listing Category
-lc = ListingCategory.create(
-    listing_id: listing1.id,
+wheel_listing_category = ListingCategory.create(
+    listing_id: wheel_listing.id,
     category_id: wheels.id
+)
+
+car_listing_category = ListingCategory.create(
+    listing_id: car_listing.id,
+    category_id: car.id
 )
