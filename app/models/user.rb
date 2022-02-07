303daff4006447872eebfd489eda7ced5c30579f
Sam <samara.amanda01@gmail.com>
2022-02-07 18:35:00 -0500

    google oauth implemented, user methods setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :listings
+    validates :email, presence: true
+    validates :email, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+  resources :parts_listings
+  resources :car_listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/users', to: 'users#create'
+  get '/users/:user_id', to: 'users#show'
+  get '/users', to: 'users#index'
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
+
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
-      resources :listings, only: [:index]
+      resources :users, only: [:create, :show, :index]
+      resources :listings, only: [:index, :create, :show]
       resources :categories, only: [:index]
     end
   end
