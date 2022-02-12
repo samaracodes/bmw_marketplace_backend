2a6952d344536726e9ee3f7913e294133dbee3de
Sam <samara.amanda01@gmail.com>
2022-02-11 20:12:39 -0500

    added listing serializer attribute to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email, :birthday, :location
-  has_many :listings
+  has_many :listings, serializer: ListingSerializer  
+
 end
