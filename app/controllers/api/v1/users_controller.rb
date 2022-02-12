913b78711ae4e09f96f12ec47dfe44f17935b91a
Sam <samara.amanda01@gmail.com>
2022-02-11 20:04:55 -0500

    corrected user error by returning user object instead of serialized user object

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,52 +1,33 @@
 class Api::V1::UsersController < ApplicationController
-    
+
     def index
-        @users = User.all
-           if @users
-              render json: {
-              users: @users
-           }
-          else
-              render json: {
-              status: 500,
-              errors: ['no users found']
-          }
-         end
+        users = User.all
+        render json: UserSerializer.new(users)
+    end
+
+
+    def create
+        @user = User.new(user_params)
+        
+        if @user.save
+            session[:user_id] = @user.id
+            render json: { user: @user, status: :created}
+        else 
+            render json: {error: 'Something went wrong. Please try again.'}
+        end
     end
 
     def show
-        @user = User.find_by(params[:id])
-            if @user
-                render json: {
-                user: @user
-            }
-            else
-                render json: {
-                status: 500,
-                errors: ['user not found']
-                }
-            end
-    end
-      
-    def create
-         @user = User.new(user_params)
-             if @user.save
-                 login!  
-                 render json: {
-                 status: :created,
-                 user: @user
-             }
-            else 
-                render json: {
-                status: 500,
-                errors: @user.errors.full_messages
-            }
-            end
+        user = User.find_by_id(params[:id])
     end
 
     private
-      
+
     def user_params
-         params.require(:user).permit(:name, :email, :password, :birthday, :location)
+        params.require(:user).permit(:name, :email, :password, :location, :birthday)
+    end
+
+    def set_user
+        @user = User.find_by_id(params[:id])
     end
 end
