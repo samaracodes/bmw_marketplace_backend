d7cb909fd7f00c2fba7925e669994e697f58bb9b
Sam <samara.amanda01@gmail.com>
2022-03-23 10:38:57 -0400

    bug fix'

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_213147) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_07_24_213147) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2021_07_24_213147) do
     t.string "listing_type"
     t.string "duration"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
